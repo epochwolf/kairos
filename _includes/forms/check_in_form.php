b5f5a4c861c04f924df888a6bd33684bc8bd0f4f
epochwolf <epochwolf@gmail.com>
2015-10-05 13:38:15 -0400

    Adding birthdate to check in.

--- _includes/forms/check_in_form.php
+++ _includes/forms/check_in_form.php
@@ -6,6 +6,7 @@ class CheckInForm extends BaseForm{
 
   function __construct($params=[]){
     $this->attendee = Attendee::find($params["id"]);
+    $this->params["birthdate"] = $this->attendee->birthdate;
     $this->params["badge_name"] = $this->attendee->badge_name;
     $this->params["badge_number"] = $this->attendee->badge_number;
     $this->params["badge_type"] = $this->attendee->badge_type;
@@ -13,6 +14,11 @@ class CheckInForm extends BaseForm{
   }
 
   public function validate(){
+    $this->error_if_empty("birthdate");
+    if(!$this->error_on("birthdate")){
+      $this->error_if_invalid_date("birthdate");
+    }
+
     $this->error_if_empty("badge_name");
     if(!$this->error_on("badge_name")){
       if(!Attendee::is_unique_badge_name(@$this->params["badge_name"], $this->attendee)){
